62e6631733d77b3d8dd96aabaf1bdae3ae0e09a2
Carlos Fernandez <cafl.23w@gmail.com>
2022-04-22 22:24:19 +0200

    got the rest countries api to work. needs formatting and stuff

--- src/pageComponents/LearnPage.jsx
+++ src/pageComponents/LearnPage.jsx
@@ -1,55 +1,93 @@
-import { useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 
-const sampleCard = {
-  countryName: "Norway",
-  description:
-    "Norway is a really cool country and I would love to go there and climb and stuf256x192256x192256x192256x192256x192256x192256x192256x12256x192f",
-  flag: "https://flagcdn.com/192x144/no.png",
-  coin: "crown",
-  population: "shit ton of people",
+const blankCountry = {
+  name: {
+    common: "",
+  },
+  capital: [],
+  languages: {},
+  currencies: {},
 };
 
 function CountryCard(props) {
   return (
     <Card className="m-3 p-2">
-      <Card.Img style={{ width: "10rem" }} variant="top" src={props.c.flag} />
+      <Card.Img
+        style={{ width: "10rem" }}
+        variant="top"
+        src={props.info.flags.png}
+      />
       <Card.Body>
         <Card.Title>
-          <h1>{props.c.countryName}</h1>
+          <h1>{props.info.name.common}</h1>
         </Card.Title>
-        <Card.Text>{props.c.description}</Card.Text>
+        <Card.Text>{props.descr}</Card.Text>
       </Card.Body>
     </Card>
   );
 }
 
-async function handleClick() {
-  const res = await fetch("https://www.randomAPI.com");
-  console.log(res.json());
-}
-
 function LearnPage() {
-  const [searchRes, setSearchRes] = useState([sampleCard]);
   const [currentSearchBar, setCurrentSearchBar] = useState("");
+  const [countryData, setCountryData] = useState(blankCountry);
+  const [isLoading, setLoading] = useState(false);
+
+  function handleChange(event) {
+    setCurrentSearchBar(event.target.value);
+  }
+  async function handleClick(e) {
+    e.preventDefault();
+    setLoading(true);
+    //const res = await fetch(`locahost:3001/getCountryInfo/${currentSearchBar}`);
+    try {
+      console.log(currentSearchBar.toLowerCase());
+      const res = await fetch(
+        `https://restcountries.com/v3.1/name/${currentSearchBar.toLowerCase()}`
+      );
+      const infoRes = await res.json();
+
+      setCountryData(infoRes[0]);
+      setLoading(false);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <div className="col-9 w-fit">
       <h2>Learn</h2>
       <hr />
       <div className="container content-center items-center flex">
-        <p>Type a country you would like to learn about: </p>
-        <form className="align-middle flex">
+        <form className="align-middle flex" onSubmit={handleClick}>
           <input
             className="whitespace-pre-line h-4"
             placeholder="Just spell it correctly"
-            onChange={(e) => setCurrentSearchBar(e.target.value)}
+            onChange={handleChange}
           />
-          <button onClick={handleClick} className="m-3 btn btn-outline-info">
-            Search
-          </button>
+          <button className="m-3 btn btn-outline-info">Search</button>
         </form>
-        <CountryCard c={sampleCard} />
+        <button
+          onClick={(e) => {
+            setCountryData(blankCountry);
+          }}
+          className="m-3 btn btn-outline-info"
+        >
+          Clear
+        </button>
       </div>
+      {isLoading || countryData == blankCountry ? (
+        ""
+      ) : (
+        <Fragment>
+          <h1>{countryData["name"]["common"]}</h1>
+          <ul>
+            <li>Capital: {countryData["capital"][0]}</li>
+            <li>Currency: {JSON.stringify(countryData["currencies"])}</li>
+            <li>Languages: {JSON.stringify(countryData["languages"])}</li>
+          </ul>
+        </Fragment>
+      )}
     </div>
   );
 }
